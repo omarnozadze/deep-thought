c1df79d63aada475630abe87120148f8eeff6c93
omarnozadze <127139782+omarnozadze@users.noreply.github.com>
2023-03-10 13:07:28 +0400

    Add files via upload

---
+++ deep-thought.py
@@ -0,0 +1,13 @@
+answer = input(('What is the Answer to the Great Question of Life, the Universe, and Everything?'))
+a='42'
+b='forty-two'
+c='forty two'
+
+if answer == a:
+    print ("YES")
+elif answer ==b:
+    print ("YES")
+elif answer==c:
+    print ("YES")
+else:
+    print ("NO")
